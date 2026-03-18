7624a25df000558a0f5cf5a9a328f8569fb808ca
Victor Wilk <victor.wilk2@mail.dcu.ie>
2026-03-18 20:59:04 +0000

    Fixed paths.

--- html/js/pgp.js
+++ html/js/pgp.js
@@ -1,5 +1,5 @@
 function givePgpKey() {
-    fetch("../../../data/VWilk_0x3871C00A9A787DB8_public.asc")
+    fetch("../../data/VWilk_0x3871C00A9A787DB8_public.asc")
         .then((res) => res.text())
         .then((text) => {
             navigator.clipboard.writeText(text);
